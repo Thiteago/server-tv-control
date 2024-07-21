8440c4b99d019b7f43eb199c5bb8abf4d4f246d3
Thiago <thiagodavidpd@gmail.com>
2024-07-21 17:32:11 -0300

    refactor: put .env for easyly change local ip

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
+from dotenv import load_dotenv
+import os
 
 app = create_app()
+load_dotenv()
 
 if __name__ == '__main__':
-  app.run(host='192.168.1.3', port=5000)
+  app.run(host=f"{os.getenv('IP_LOCAL_MACHINE')}", port=5000)
