b29c6e018d7993b66205d0449ec2b99c563d472c
Thiago <thiagodavidpd@gmail.com>
2024-07-25 00:07:20 -0300

    fix: mute initial page audio from netflix and change time sleeps count

--- app/services/crawlers/netflix_crawler.py
+++ app/services/crawlers/netflix_crawler.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 import time
 import os
@@ -11,23 +13,31 @@ class NetflixCrawler:
 
   def login(self):
     self.driver.get('https://www.netflix.com/login')
-    time.sleep(5)
+    time.sleep(2)
 
     email_input = self.driver.find_element(By.CSS_SELECTOR, '[data-uia="login-field"]')
     email = os.getenv('NETFLIX_EMAIL')
-    time.sleep(5)
+    time.sleep(2)
     email_input.send_keys(email)
     password_input = self.driver.find_element(By.CSS_SELECTOR, '[data-uia="password-field"]')
     password_input.send_keys(os.getenv('NETFLIX_PASSWORD'))
     login_button = self.driver.find_element(By.CSS_SELECTOR, '[data-uia="login-submit-button"]')
     login_button.click()
-    time.sleep(5)
+    time.sleep(2)
     profile = self.driver.find_element(By.CSS_SELECTOR, '[data-profile-guid="G3CN4XONGZHF3CJKMNUFR6NVDM"]')
     profile.click()
+    time.sleep(2)
+
+    WebDriverWait(self.driver, 10).until(
+      EC.presence_of_element_located((By.CSS_SELECTOR, '[data-uia="audio-toggle-unmuted"]'))
+    )
+    
+    mute_audio_btn = self.driver.find_element(By.CSS_SELECTOR, '[data-uia="audio-toggle-unmuted"]')
+    mute_audio_btn.click()
 
   def verifyLogin(self):
     self.driver.execute_script("window.open('');")
-    self.driver.switch_to.window(self.driver.window_handles[3])
+    self.driver.switch_to.window(self.driver.window_handles[1])
     self.driver.get('https://www.netflix.com/')
     try:
       self.driver.find_element(By.CSS_SELECTOR, '[data-profile-guid="G3CN4XONGZHF3CJKMNUFR6NVDM"]')
